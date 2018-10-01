28f06578b74387b0f2b0d7a99afc9c85a3eb28bb
Marc H. Weiner <marcw@elementalcognition.com>
2018-10-01 16:11:04 -0400

    add first and last methods

--- src/index.js
+++ src/index.js
@@ -8,79 +8,135 @@ function OrderedDict() {
 OrderedDict.prototype = {
 
   set: function(key, val) {
+
     if (!(key in this.dict)) {
+
       this.arr.push(key);
+
     }
+
     this.dict[key] = val;
+
     return this.dict[key];
+
   },
 
-  get: function(key){
+  get: function(key) {
+
     return this.dict[key];
+
   },
 
-  nth: function(index){
+  nth: function(index) {
+
     return this.dict[this.arr[index]];
+
+  },
+
+  first: function() {
+
+    return this.dict[this.arr[0]];
+
+  },
+
+  last: function() {
+
+    return this.dict[this.arr[this.arr.length - 1]];
+
   },
 
   has: function(key){
+
     return !!this.dict[key];
+
   },
 
-  remove: function(key){
+  remove: function(key) {
+
     for (let i = 0, l = this.arr.length; i < l; i++) {
+
       if (this.arr[i] === key) {
+
         this.arr.splice(i, 1);
+
         delete this.dict[key];
+
       }
     }
+
   },
 
-  insert: function(index, key, val){
-    if(this.arr.indexOf(key) !== -1) throw 'key `' + key + '` already exists';
+  insert: function(index, key, val) {
+
+    if (this.arr.indexOf(key) !== -1) throw 'key `' + key + '` already exists';
+
     this.arr.splice(index, 0, key);
     this.dict[key] = val;
+
   },
 
-  forEach: function(callback){
+  forEach: function(callback) {
+
     for (let i = 0, l = this.arr.length; i < l; i++) {
+
       let key = this.arr[i],
           val = this.dict[key];
+
       callback(val, key);
+
     }
+
   },
 
-  sort: function(){
+  sort: function() {
+
     //to be overridden
+
   },
 
-  size: function(){
+  size: function() {
+
     return this.arr.length;
+
   },
 
-  keys: function(){
+  keys: function() {
+
     return this.arr;
+
   },
 
-  values: function(){
+  values: function() {
+
     let vals = [],
       len = this.arr.length;
-    for(let i = 0; i < len; i++){
+
+    for (let i = 0; i < len; i++) {
+
       vals.push(this.dict[this.arr[i]]);
+
     }
+
     return vals;
+
   },
 
-  clear: function(){
+  clear: function() {
+
     this.dict = {};
     this.arr = [];
+
   },
 
-  clone: function(){
+  clone: function() {
+
     let d = new OrderedDict();
+
     d.dict = JSON.parse(JSON.stringify(this.dict));
     d.arr = JSON.parse(JSON.stringify(this.arr));
+
     return d;
+
   }
 };
 
